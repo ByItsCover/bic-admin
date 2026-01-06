baca2f2acb969ee018f0c53e34a31c4b0103127c
Kalamojo <kojo22444488888888@gmail.com>
2026-01-05 23:32:55 -0500

    Using separate workspace for admin

--- versions.tf
+++ versions.tf
@@ -1,7 +1,22 @@
+/*
 terraform {
   required_providers {
     tfe = {
       version = "~> 0.72.0"
     }
   }
+}
+*/
+
+terraform {
+  cloud {
+    organization = "ByItsCover"
+
+    workspaces {
+      project = "ByItsCover_Admin"
+      name    = "bic-admin"
+    }
+  }
+
+  required_version = ">= 1.2"
 }
